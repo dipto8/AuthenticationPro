2d10f59df681a267c35de2c1c24fdeb6e502fcca
FAHAD KABIR <hmdipto7@gmail.com>
2024-10-23 15:20:16 -0230

    onAuthStateChanged

--- AuthenticationPro/src/ContextProvider/ContextProvider.jsx
+++ AuthenticationPro/src/ContextProvider/ContextProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from "react";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import auth from "../../firebase/Firebase.config";
 
 export const AppContext = createContext(null);
@@ -14,6 +14,15 @@ const createUser = (email, password) => {
 const userSignIn =(email, password)=>{
     return signInWithEmailAndPassword(auth,email,password)
 }  
+
+useEffect(()=>{
+    onAuthStateChanged(auth,(currentUser)=>{
+        setUser(currentUser);
+        console.log( currentUser, 'curent User')
+
+    }):
+    const unscribe
+},[])
 
   const userInfo = { user, createUser, userSignIn };
   return <AppContext.Provider value={userInfo}>{children}</AppContext.Provider>;
